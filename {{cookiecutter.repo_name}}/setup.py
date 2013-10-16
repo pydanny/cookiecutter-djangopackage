979ba98694b89667974a7231efd0a3d0add70407
Federico Frenguelli <synasius@gmail.com>
2013-10-16 15:44:50 +0200

    Fixed package name in setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -4,14 +4,14 @@
 import os
 import sys
 
-import {{ cookiecutter.repo_name }}
+import {{ cookiecutter.app_name }}
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-version = {{ cookiecutter.repo_name }}.__version__
+version = {{ cookiecutter.app_name }}.__version__
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
@@ -32,7 +32,7 @@ setup(
     author_email='{{ cookiecutter.email }}',
     url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
     packages=[
-        '{{ cookiecutter.repo_name }}',
+        '{{ cookiecutter.app_name }}',
     ],
     include_package_data=True,
     install_requires=[
