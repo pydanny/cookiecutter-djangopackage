95e543b8fcf4761b24800d518afaee620fd1c159
Paul Hallett <hello@phalt.co>
2014-06-16 15:26:32 +0100

    Replaced TestCase
    Replaced TestCase with Django’s own TestCase.
    
    This is likely to be favoured by most users of this package, so I
    thought having the default set to Django’s TestCase would be preferable.

--- {{cookiecutter.repo_name}}/tests/test_models.py
+++ {{cookiecutter.repo_name}}/tests/test_models.py
@@ -10,12 +10,12 @@ Tests for `{{ cookiecutter.repo_name }}` models module.
 
 import os
 import shutil
-import unittest
+from django.test import TestCase
 
 from {{ cookiecutter.app_name }} import models
 
 
-class Test{{ cookiecutter.app_name|capitalize }}(unittest.TestCase):
+class Test{{ cookiecutter.app_name|capitalize }}(TestCase):
 
     def setUp(self):
         pass
