f7e2aba58726325cd0cfdbd3f6e6a8e567878be1
leportella <leportella@gmail.com>
2016-06-02 19:27:30 -0300

    [WIP] adding views (create, delete, etc...) for each model and urls for each view

--- {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/urls.py
@@ -1,6 +1,25 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
+from . import views
+
+{% set views_names = ['Create', 'Delete', 'Detail', 'Update', 'List'] -%}
+
 urlpatterns = [
     url(r'', TemplateView.as_view(template_name="base.html")),
+
+	{% for model in cookiecutter.models.split(',') -%}
+	{% for view in views_names -%}
+
+    url(
+        regex = "^{{ model.strip() }}/~{{ view.lower() }}$",
+        view = views.{{ model.strip() }}{{ view }}View.as_view(),
+        name = '{{ model.strip() }}_{{ view.lower() }}',
+	),
+
+    {% endfor %}
+	{% endfor -%}
+
 ]
+
---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/views.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    UpdateView,
+    ListView
+)
+
+from .models import ({% for model in cookiecutter.models.split(',') %}
+	{{ model.strip() }},{% endfor %}
+)
+
+{% set views = [
+	'CreateView',
+	'DeleteView',
+	'DetailView',
+	'UpdateView',
+	'ListView'
+] -%}
+
+{% for model in cookiecutter.models.split(',') -%}
+{% for view in views %}
+class {{ model }}{{ view }}({{ view }}):
+
+    model = {{ model }}
+
+{% endfor -%}
+{% endfor %}
