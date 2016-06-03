1067972e6530afc7296fe8d4d42e0ac11fa4730f
leportella <leportella@gmail.com>
2016-06-03 15:15:40 -0300

    urls treatment depending on if any models exits and private keys in some methods

--- {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/urls.py
@@ -4,22 +4,37 @@ from django.views.generic import TemplateView
 
 from . import views
 
-{% set views_names = ['Create', 'Delete', 'Detail', 'Update', 'List'] -%}
-
 urlpatterns = [
+    {% if cookiecutter.models == "Comma-separated list of models" -%}
     url(r'', TemplateView.as_view(template_name="base.html")),
-
+    {% else -%}
 	{% for model in cookiecutter.models.split(',') -%}
-	{% for view in views_names -%}
-
     url(
-        regex = "^{{ model.strip() }}/~{{ view.lower() }}$",
-        view = views.{{ model.strip() }}{{ view }}View.as_view(),
-        name = '{{ model.strip() }}_{{ view.lower() }}',
-	),
-
-    {% endfor %}
+        regex = "^{{ model.strip() }}/~create/$",
+        view = views.{{ model.strip() }}CreateView.as_view(),
+        name = '{{ model.strip() }}_create',
+    ),
+    url(
+        regex = "^{{ model.strip() }}/(?P<pk>\d+)/~delete/$",
+        view = views.{{ model.strip() }}DeleteView.as_view(),
+        name = '{{ model.strip() }}_delete',
+    ),
+    url(
+        regex = "^{{ model.strip() }}/(?P<pk>\d+)/$",
+        view = views.{{ model.strip() }}DetailView.as_view(),
+        name = '{{ model.strip() }}_detail',
+    ),
+    url(
+        regex = "^{{ model.strip() }}/(?P<pk>\d+)/~update/$",
+        view = views.{{ model.strip() }}UpdateView.as_view(),
+        name = '{{ model.strip() }}_update',
+    ),
+    url(
+        regex = "^{{ model.strip() }}/$",
+        view = views.{{ model.strip() }}ListView.as_view(),
+        name = '{{ model.strip() }}_list',
+    ),
 	{% endfor -%}
-
+    {% endif %}
 ]
 
