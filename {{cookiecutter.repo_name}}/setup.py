1433663de864ea688d0c767f9a377fa41a8e3fda
Daniel Roy Greenfeld <pydanny@users.noreply.github.com>
2016-08-07 12:40:39 -0700

    Changed GitHub exclusivity to all package repo systems

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -29,7 +29,7 @@ if sys.argv[-1] == 'publish':
     sys.exit()
 
 if sys.argv[-1] == 'tag':
-    print("Tagging the version on github:")
+    print("Tagging the version on git:")
     os.system("git tag -a %s -m 'version %s'" % (version, version))
     os.system("git push --tags")
     sys.exit()
