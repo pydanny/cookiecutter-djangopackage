89157eef71a16b9d60959141de09ce59cdf90b3c
Daniel Roy Greenfeld <pydanny@users.noreply.github.com>
2016-12-06 20:42:38 -0800

    Re-added get_version
    I've found this version works great.

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import re
 import sys
 
 try:
@@ -15,7 +16,19 @@ except ImportError:
     'MIT': 'License :: OSI Approved :: MIT License',
 } %}
 
-version = '{{ cookiecutter.version }}'
+
+def get_version(*file_paths):
+    """Retrieves the version from {{ cookiecutter.app_name }}/__init__.py"""
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+version = get_version("{{ cookiecutter.app_name }}", "__init__.py")
+
 
 if sys.argv[-1] == 'publish':
     try:
