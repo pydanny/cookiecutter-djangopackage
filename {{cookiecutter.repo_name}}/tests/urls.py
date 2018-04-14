a80111fde159364d3a0c38c0567cfab46478229a
John Carter <john@therefromhere.org>
2018-04-14 14:32:12 +1200

    Updated urls.py to be compatible with both Django 1.11 and Django 2.0
    https://docs.djangoproject.com/en/2.0/topics/http/urls/#id5

--- {{cookiecutter.repo_name}}/tests/urls.py
+++ {{cookiecutter.repo_name}}/tests/urls.py
@@ -3,8 +3,7 @@ from __future__ import unicode_literals, absolute_import
 
 from django.conf.urls import url, include
 
-from {{ cookiecutter.app_name }}.urls import urlpatterns as {{ cookiecutter.app_name }}_urls
 
 urlpatterns = [
-    url(r'^', include({{ cookiecutter.app_name }}_urls, namespace='{{ cookiecutter.app_name }}')),
+    url(r'^', include('{{ cookiecutter.app_name }}.urls', namespace='{{ cookiecutter.app_name }}')),
 ]
