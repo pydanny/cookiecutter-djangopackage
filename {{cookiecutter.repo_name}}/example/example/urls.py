4f4edfd843729acd1cf65e09bf0615e973dc120b
Pavel Tanchev <dcopm999@gmail.com>
2019-09-28 13:30:24 +0500

    Change execute old url to path

--- {{cookiecutter.repo_name}}/example/example/urls.py
+++ {{cookiecutter.repo_name}}/example/example/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('{{ cookiecutter.app_name }}.urls', namespace='{{ cookiecutter.app_name }}')),
+    path('admin/', admin.site.urls),
+    path('', include('{{ cookiecutter.app_name }}.urls', namespace='{{ cookiecutter.app_name }}')),
 ]
