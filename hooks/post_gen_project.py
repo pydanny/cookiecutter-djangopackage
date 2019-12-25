c1b5b07311f37257cdd362a12eba1a7708a6cf2b
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-12-25 11:28:51 +0100

    Move example application to a test_utils directory

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,14 +8,17 @@ import shutil
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
+
 def remove_example_project(project_directory):
     """Removes the taskapp if celery isn't going to be used"""
     # Determine the local_setting_file_location
     location = os.path.join(
         PROJECT_DIRECTORY,
+        'tests',
         'example'
     )
     shutil.rmtree(location)
+
 
 # 1. Removes the example project if it isn't going to be used
 if '{{ cookiecutter.create_example_project }}'.lower() == 'n':
--- {{cookiecutter.repo_name}}/tests/urls.py
+++ {{cookiecutter.repo_name}}/tests/urls.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals, absolute_import
 
 from django.conf.urls import url, include
-
+from django.contrib import admin
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^', include('{{ cookiecutter.app_name }}.urls', namespace='{{ cookiecutter.app_name }}')),
 ]
