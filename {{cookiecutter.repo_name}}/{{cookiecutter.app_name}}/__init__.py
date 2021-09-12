c8d7d0774534778d0baa96b0483fe1e7816edf12
master <dcopm999@gmail.com>
2021-09-12 18:56:19 +0500

    {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/__init__.py: Changed default_app_config

--- {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/__init__.py
@@ -1,2 +1,2 @@
 __version__ = "{{ cookiecutter.version }}"
-default_app_config = "{{ cookiecutter.project_name }}.apps.{{ cookiecutter.app_config_name }}"
+default_app_config = "{{ cookiecutter.app_name }}.apps.{{ cookiecutter.app_config_name }}"
