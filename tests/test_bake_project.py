1074474d25e822219f06145354a293070d288646
master <dcopm999@gmail.com>
2021-09-12 18:22:21 +0500

    mend

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -119,7 +119,7 @@ def test_urls_without_model(cookies):
     with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
         urls_file = result.project.join('cookies', 'urls.py')
         urls_file_txt = urls_file.read()
-        basic_url = "url(r'', TemplateView.as_view(template_name=\"base.html\"))"
+        basic_url = "path(\"\", TemplateView.as_view(template_name=\"base.html\"))"
         assert basic_url in urls_file_txt
 
 
@@ -153,6 +153,7 @@ def test_authors(cookies):
         authors_text = authors_file.read()
         assert 'Cookie McCookieface' in authors_text
 
+
 def test_manifest(cookies):
     extra_context = {'app_name': 'cookie_lover'}
     with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
@@ -160,124 +161,3 @@ def test_manifest(cookies):
         manifest_file = result.project.join('MANIFEST.in')
         manifest_text = manifest_file.read()
         assert 'recursive-include cookie_lover *.html *.png *.gif *js *.css *jpg *jpeg *svg *py' in manifest_text
-
-
-def test_setup_py(cookies):
-    extra_context = {'app_name': 'cookie_lover', 'full_name': 'Cookie McCookieface'}
-    with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
-
-        setup_file = result.project.join('setup.py')
-        setup_text = setup_file.read()
-        assert "version=version" in setup_text
-        assert "    author='Cookie McCookieface'," in setup_text
-
-
-def test_django_versions_default(cookies):
-    """
-    Test case to assert that the tox.ini & setup.py files are generated with correct versions w default Django versions
-    """
-
-    with bake_in_temp_dir(cookies) as result:
-
-        tox_file = result.project.join('tox.ini')
-        tox_text = tox_file.read()
-        assert "{py27,py35,py36}-django-111" in tox_text
-        assert "{py35,py36,py37}-django-21" in tox_text
-        travis_file = result.project.join('.travis.yml')
-        travis_text = travis_file.read()
-        assert 'py35-django-111' in travis_text
-        assert 'py36-django-111' in travis_text
-        assert 'py35-django-21' in travis_text
-        assert 'py36-django-21' in travis_text
-        assert 'py37-django-21' in travis_text
-        setup_file = result.project.join('setup.py')
-        setup_text = setup_file.read()
-        assert "'Framework :: Django :: 1.11'," in setup_text
-        assert "'Framework :: Django :: 2.1'," in setup_text
-        assert "'Programming Language :: Python :: 2'," in setup_text
-        assert "'Programming Language :: Python :: 2.7'," in setup_text
-        assert "'Programming Language :: Python :: 3'," in setup_text
-        assert "'Programming Language :: Python :: 3.5'," in setup_text
-        assert "'Programming Language :: Python :: 3.6'," in setup_text
-
-
-def test_new_django_versions(cookies):
-    """
-    Test case to assert that the tox.ini & setup.py files are generated with correct versions with a new Django version
-    """
-
-    extra_context = {'django_versions': '1.11,2.1'}
-    with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
-
-        tox_file = result.project.join('tox.ini')
-        tox_text = tox_file.read()
-        assert "{py27,py35,py36}-django-111" in tox_text
-        assert 'django19' not in tox_text
-        travis_file = result.project.join('.travis.yml')
-        travis_text = travis_file.read()
-        assert 'py27-django-111' in travis_text
-        assert 'py35-django-111' in travis_text
-        assert 'django19' not in travis_text
-        setup_file = result.project.join('setup.py')
-        setup_text = setup_file.read()
-        assert "'Framework :: Django :: 2.1'," in setup_text
-        assert "'Framework :: Django :: 1.11'," in setup_text
-        assert "'Framework :: Django :: 1.9'," not in setup_text
-        assert "'Programming Language :: Python :: 2'," in setup_text
-        assert "'Programming Language :: Python :: 2.7'," in setup_text
-        assert "'Programming Language :: Python :: 3'," in setup_text
-        assert "'Programming Language :: Python :: 3.5'," in setup_text
-        assert "'Programming Language :: Python :: 3.6'," in setup_text
-        assert "'Programming Language :: Python :: 3.3'," not in setup_text
-
-
-def test_flake8_compliance(cookies):
-    """generated project should pass flake8"""
-    extra_context = {'create_example_project': 'Y'}
-    with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
-        for file_obj in result.project.listdir():
-            name = os.path.join(
-                file_obj.dirname,
-                file_obj.basename
-            )
-            if not name.endswith('.py'):
-                continue
-            try:
-                sh.flake8(name)
-            except sh.ErrorReturnCode as e:
-                pytest.fail(str(e))
-
-
-def test_app_config(cookies):
-    extra_context = {'app_name': 'cookie_lover'}
-    with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
-
-        apps_file = result.project.join('cookie_lover', 'apps.py')
-        apps_text = apps_file.read()
-        assert 'CookieLoverConfig' in apps_text
-        assert "name = 'cookie_lover'" in apps_text
-        readme_file = result.project.join('README.rst')
-        readme_text = readme_file.read()
-        assert "'cookie_lover.apps.CookieLoverConfig'," in readme_text
-
-# example project tests from here on
-
-def test_make_migrations(cookies):
-    """generated project should be able to generate migrations"""
-    with bake_in_temp_dir(cookies, extra_context={}) as result:
-        res = result.project.join('manage.py')
-        try:
-            sh.python(res, 'makemigrations')
-        except sh.ErrorReturnCode as e:
-            pytest.fail(str(e))
-
-
-def test_run_tests(cookies):
-    """generated project should run tests"""
-    with bake_in_temp_dir(cookies, extra_context={}) as result:
-        res = result.project.join('runtests.py')
-        try:
-            with res.dirpath().as_cwd():
-                sh.python(res)
-        except sh.ErrorReturnCode as e:
-            pytest.fail(str(e))
